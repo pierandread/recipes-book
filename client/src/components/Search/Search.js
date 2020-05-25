fddc08d15c4e1d7fd8e66a6f1defddff0443c2c7
Pier Andrea <pierandreadelise@gmail.com>
2020-05-25 13:32:14 +0200

    feat: services with api call using everywhere cors

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -2,6 +2,7 @@ import React, { useState }  from 'react';
 import TextField from '@material-ui/core/TextField';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import SearchIcon from '@material-ui/icons/Search';
+import getRecipies from '../../services/recepypuppyCall';
 
 
 
@@ -9,13 +10,19 @@ function Search() {
 
   const [searchInput, setSearchInput] = useState(null);
 
+  const setInputs = (event) => {
+    event.preventDefault(); 
+    setSearchInput(event.target.value);
+  }
+
   return(
     <div>
       <TextField 
         id="search-input" 
         label="Search" 
         variant="outlined" 
-        autocomplete="off"
+        autoComplete="off"
+        helperText="Search at least 3 characters"
         InputProps={{
           endAdornment: (
             <InputAdornment position="end">
@@ -23,6 +30,8 @@ function Search() {
             </InputAdornment>
           ),
         }}
+        onChange = {event => setInputs(event)}
+        onKeyDown={(e) => {e.keyCode===13 && getRecipies(searchInput)}} 
         >
         
       </TextField>
---
+++ client/src/services/recepypuppyCall.js
@@ -0,0 +1,33 @@
+const apiUrl = "http://www.recipepuppy.com/api/";
+const corsOriginUrl = "https://cors-anywhere.herokuapp.com/";
+
+//to test, example considered 
+  // let str1 ="onion"
+  // let str2 = ".onion garlic"
+  // let str3 = "onion, garlic"
+  // let str4 = " onion,garlic"
+  // let str5 = 2
+
+const rightInputs = (inputs) => {
+  let res =[];
+  if (typeof inputs !== "string") throw new Error('Wrong type of input');
+  let inpWithoutSpecialChar = inputs.replace(/[^a-zA-Z ]/g, " ");
+  let arrInputs = inpWithoutSpecialChar.split(" ");
+  for (let i = 0; i<arrInputs.length; i++){
+    if (arrInputs[i]!== "") {
+      res.push(arrInputs[i])
+    }
+   }
+   return res.join();
+}
+
+export default async function getRecipies(inputs) {
+  inputs.length<3 && alert("Please insert more than 3 characters");
+  const parameters = rightInputs(inputs);
+  const fetchUrl = `${corsOriginUrl}${apiUrl}?i=${parameters}&p=1`
+  const response = await fetch(fetchUrl, {referrerPolicy: "no-referrer-when-downgrade"})
+    .then(res => res.json())
+    .catch(error => console.log(error));
+  console.log(response)
+  return response       
+}
