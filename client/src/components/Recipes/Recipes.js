242d416f9b623066a5f4beb80955e867eb800c1b
Pier Andrea <pierandreadelise@gmail.com>
2020-05-25 19:25:29 +0200

    feat: recipe component + label style

--- client/src/components/Recipes/Recipes.js
+++ client/src/components/Recipes/Recipes.js
@@ -1,30 +1,13 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
 import Grid from '@material-ui/core/Grid';
+import Recipe from '../Recipe/Recipe'
 
 
 const useStyles = makeStyles({
-  label: {
-    transform: "rotate(45deg)"
-  },
-  recipe: {
-    boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
-    marginRight: "3%",
-    marginBottom: "2%",
-    "&:hover": {
-      boxShadow: "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)"
-    }
-  },
   recepies: {
     margin: "0 5%",
   },
-  link: {
-    textDecoration: "none",
-    color: "black"
-  }
 });
 
 function Recipes({ recipes }) {
@@ -35,33 +18,10 @@ function Recipes({ recipes }) {
     <div >
       <Grid container={true} spacing={3} className={classes.recepies}>
         {recipes && recipes.map((recipe, idx) =>
-          <Grid key={idx} item xs={12} md={5} className={classes.recipe}>
-            <a className={classes.link} href={recipe.href} target="_blank" rel="noopener noreferrer">
-              <Grid spacing={1} container={true}>
-                <Grid item xs={6}>
-                  <img alt={recipe.title} src={recipe.thumbnail} />
-                </Grid>
-                <Grid item xs={6} align="right">
-                  {(recipe.ingredients.includes("milk") || recipe.ingredients.includes("cheese")) && <Typography variant="h6" className={classes.label}>Has Lactose</Typography>}
-                </Grid>
-                <Grid item xs={12}>
-                  <Typography variant="h6">Name:</Typography>
-                  <Typography variant="body2" > {recipe.title}</Typography>
-                </Grid>
-                <Grid item xs={12}>
-                  <Typography variant="h6" >Ingredients: </Typography>
-                </Grid>
-                <Grid item xs={12} style={{paddingTop: "0"}}>
-                  <List dense={true} style={{paddingTop: "0"}}>
-                    {recipe.ingredients && recipe.ingredients.split(",").map((ingredient, idx) => <ListItem key={idx} ><Typography variant="body2">- {ingredient}</Typography></ListItem>)}
-                  </List>
-                </Grid>
-              </Grid>
-            </a>
-          </Grid>
+          <Recipe key={idx} recipe={recipe} idx={idx} />
         )}
       </Grid>
-      {(recipes && recipes.length === 0) && <p>No results, try another search</p>}
+      {(recipes && recipes.length === 0) && <p className={classes.recepies}>No results, try another search</p>}
     </div>
   )
 }
